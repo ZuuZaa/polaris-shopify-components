a289054a04f208922292f3ee8df6072f76149b88
ZuuZaa <zumbaghirova@gmail.com>
2023-06-23 15:08:42 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Page } from "@shopify/polaris";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Page title="home">
+        <h1>Welcome Home</h1>
+
+      </Page>
+
     </div>
   );
 }
